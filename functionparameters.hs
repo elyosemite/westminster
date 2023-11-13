4abac55eafb2c55ec642049a46fd7057287a08ea
Yuri Melo <yurifullstack@gmail.com>
2023-11-13 17:38:39 -0300

    Pattern Matching

---
+++ functionparameters.hs
@@ -0,0 +1,26 @@
+import System.Win32 (xBUTTON1)
+lucky :: (Integral a) => a -> String
+lucky 7 = "LUCKY NUMBER SEVEN!"
+lucky x = "Sorry, you're out of luck, pal!"
+
+factorial :: (Integral a) => a -> a
+factorial 0 = 1
+factorial n = n * factorial(n - 1)
+
+addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+first :: (a, b, c) -> a
+first (x, _, _) = x
+
+second :: (a, b, c) -> b
+second (_, y, _) = y
+
+third :: (a, b, c) -> c
+third (_, _, z) = z
+
+tell :: (Show a) => [a] -> String
+tell [] = "The list is empty"
+tell (x:[]) = "The list has one element: " ++ show x
+tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
+tell (x:y:_) = "This list is long. The first two elements are: " ++ show x ++ " and " ++ show y
