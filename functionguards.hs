a23f4c904a6ca14908535f493d4e5d00db3ca610
Yuri Melo <yurifullstack@gmail.com>
2023-11-18 14:27:56 -0300

    Build functions with let and in expression

--- functionguards.hs
+++ functionguards.hs
@@ -1,11 +1,13 @@
 bmiTell :: (RealFloat a) => a -> a -> String
 bmiTell weight height
-    | bmi <= 18.5 = "You're underweight, you emo, you!"
-    | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
-    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"
-    | otherwise   = "You're a whale, congratulations!"
+    | bmi <= skinny = "You're underweight, you emo, you!"
+    | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"
+    | bmi <= fat    = "You're fat! Lose some weight, fatty!"
+    | otherwise     = "You're a whale, congratulations!"
     where bmi = weight / height ^ 2
+          (skinny, normal, fat) = (18.5, 25.0, 30.0)
 
+-- Yuri Melo has created this function by your own
 whatMyName :: String -> String
 whatMyName name
     | name == "Brasil" = "Yuri Melo"
@@ -23,3 +25,20 @@ a `myCompare` b
     | a > b       = GT
     | a == b      = EQ
     | otherwise   = LT
+
+initials :: String -> String -> String
+initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
+    where (f:_) = firstname
+          (l:_) = lastname
+
+calcBmis :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis xs = [bmi w h | (w, h) <- xs]
+    where bmi weight height = weight / height ^2
+
+cylinder :: (RealFloat a) => a -> a -> a
+cylinder r h =
+    let sideArea = 2 * pi * r * h
+        topArea = pi * r ^ 2
+    in sideArea + 2 * topArea
+
+myCustomSquares = [let square x = x * x in (square 5, square 3, square 25)]
