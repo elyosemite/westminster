ebbf48629133fff50dc7c8593d5611fcb943fd60
Yuri Melo <yurifullstack@gmail.com>
2023-11-20 19:15:42 -0300

    Understand recursion with max function

--- recursion.hs
+++ recursion.hs
@@ -5,3 +5,15 @@ maximum' (x:xs)
     | x > maxTail = x
     | otherwise = maxTail
     where maxTail = maximum' xs
+
+-- [1, 3, 9, 4, 2]
+
+-- maxTail = 1  | maximum' = 9
+
+-- maxTail = 3  | maximum' = 9
+
+-- maxTail = 9  | maximum' = 9
+
+-- maxTail = 4  | maximum' = 4
+
+-- maxTail = 2  | maximum' = 2
