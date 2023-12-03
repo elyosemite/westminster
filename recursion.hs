3b327e2a3f746e26f1fa640cd58130ddfff5a8d5
Yuri Melo <yurifullstack@gmail.com>
2023-12-03 15:47:51 -0300

    Add Function

--- recursion.hs
+++ recursion.hs
@@ -17,3 +17,9 @@ maximum' (x:xs)
 -- maxTail = 4  | maximum' = 4
 
 -- maxTail = 2  | maximum' = 2
+
+-- More readable
+maximumV2 :: (Ord a) => [a] -> a
+maximumV2 [] = error "maximum of empty list"
+maximumV2 [x] = x
+maximumV2 (x:xs) = max x (maximumV2 xs)
