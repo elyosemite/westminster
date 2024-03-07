696a50775515f9f9d58aa71f7bb7dee0b8fed916
Yuri Melo <yuri.melo@trin.ca>
2024-03-07 15:48:57 -0300

    Take function with recursion

--- chapter_05/recursion.hs
+++ chapter_05/recursion.hs
@@ -31,6 +31,6 @@ replicate' n x
 
 take' :: (Num i, Ord i) => i -> [a] -> [a]
 take' n _
-    | n <= 0 = []
-take' _ []   = []
+    | n <= 0  = []
+take' _ []    = []
 take' n (x:xs) = x : take' (n-1) xs
