4de3d4bedd49bc6dfdc5697603efc65c356b9593
Yuri Melo <11520745+elyosemite@users.noreply.github.com>
2024-10-12 22:14:06 -0300

    Add lambdas

--- chapter_06/lambdas.hs
+++ chapter_06/lambdas.hs
@@ -16,3 +16,56 @@ sumTwoNumbers xs y = map (\x -> x + y) xs
 -- Fica bem mais legível
 sumTwoNumbersWithoutLambda :: [Int] -> Int -> [Int]
 sumTwoNumbersWithoutLambda xs y = map (+ y) xs
+
+-- addThree :: (Num a) => a -> a -> a -> a
+-- addThree x y z = x + y + z
+
+-- Similar a esta função abaixo com lambdas
+addThree :: (Num a) => a -> a -> a -> a
+addThree = \x -> \y -> \z -> x + y + z
+
+sum' :: (Num a) => [a] -> a
+sum' xs = foldl (\acc x -> acc + x) 0 xs
+
+-- Exercícios Básicos
+
+-- 1. Soma de uma lista:
+sumElementsInArray :: (Num a) => [a] -> a
+sumElementsInArray = foldl (+) 0
+
+-- 2. Produto de uma lista
+productElementsInArray :: (Num a) => [a] -> as
+productElementsInArray = foldl (*) 1
+
+-- 3. Verificação de todos verdadeiros:
+allTrue :: [Bool] -> Bool
+allTrue = foldl (&&) True -- Você pode substituir por apenas allTrue = and
+
+-- 4. Concatenar strings:
+concatenar :: [String] -> String
+concatenar = foldl (++) ""
+
+-- 5. Maior número:
+maxNumber :: [Int] -> Int
+maxNumber = foldl max (minBound :: Int)
+
+-- 6. Menor número:
+minNumber :: [Int] -> Int
+minNumber = foldl min (maxBound :: Int)
+
+-- 7. Conta elementos:
+countElements :: [a] -> Int
+countElements = foldl (\acc _ -> acc + 1) 0
+
+-- 8. Verificar se contém zero:
+countZero :: [Int] -> Bool
+countZero = foldr (\x acc -> x == 0 || acc) False
+
+-- 9. Reverter lista:
+revert :: [a] -> [a]
+revert = foldl (flip (:)) []
+
+-- 10. Aplicar operação condicional:
+sumPositivies :: [Int] -> Int
+sumPositivies = foldr (\x acc -> if x > 0 then x + acc else acc) 0
+
