49183bdac33e6b920dd9939e8c4366feeaff5fec
Yuri Melo <yurifullstack@gmail.com>
2025-04-25 19:52:40 -0300

    Refactor User data type formatting and improve createUser function readability

--- Domain/User.hs
+++ Domain/User.hs
@@ -2,13 +2,19 @@ module Domain.User where
 
 import Data.Time.Clock (UTCTime)
 import Domain.ValueObjects
+import Domain.ValueObject.Email
 
 data User = User
-    { userId        :: UserId
-    , userName      :: Name
-    , userEmail     :: Email
-    , userCreatedAt :: UTCTime
-} deriving (Show, Eq)
+  { userId :: UserId,
+    userName :: Name,
+    userEmail :: Email,
+    userCreatedAt :: UTCTime
+  }
+  deriving (Show, Eq)
 
 createUser :: String -> String -> UTCTime -> User
-createUser name email createdAt = User (UserId $ show createdAt) (Name name) (Email email) createdAt
+createUser name email createdAt =
+  let userIdentifier = UserId $ show createdAt
+      username = Name name
+      username_email = Email email
+   in User userIdentifier username username_email createdAt
