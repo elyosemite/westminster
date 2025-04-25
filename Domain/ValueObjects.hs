777e2d1e5e29d6f39256ec5fef51ee9a38da5b1b
Yuri Melo <yurifullstack@gmail.com>
2025-04-25 19:52:52 -0300

    Remove Email and Balance value objects from Domain.ValueObjects module

--- Domain/ValueObjects.hs
+++ Domain/ValueObjects.hs
@@ -2,6 +2,4 @@ module Domain.ValueObjects where
 
 newtype UserId = UserId String deriving (Show, Eq)
 newtype AccountNumber = AccountNumber String deriving (Show, Eq)
-newtype Email = Email String deriving (Show, Eq)
 newtype Name = Name String deriving (Show, Eq)
-newtype Balance = Balance Double deriving (Show, Eq, Ord)
