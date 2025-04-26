d131ab2bf55e6a7d9c10c30528fccbe102f872ef
Yuri Melo <yurifullstack@gmail.com>
2025-04-26 17:14:11 -0300

    Add User module with data types and createUser function

---
+++ Domain/Entity/User.hs
@@ -0,0 +1,26 @@
+module Domain.Entity.User
+(
+  User (..),
+  createUser,
+)
+where
+
+import Data.Time.Clock (UTCTime)
+import Domain.ValueObject.UserId
+import Domain.ValueObject.Email
+import Domain.ValueObject.Name
+
+data User = User
+  { userId :: UserId,
+    userName :: Name,
+    userEmail :: Email,
+    userCreatedAt :: UTCTime
+  }
+  deriving (Show, Eq)
+
+createUser :: String -> String -> UTCTime -> User
+createUser name email createdAt =
+  let userIdentifier = UserId $ show createdAt
+      username = Name name
+      username_email = Email email
+   in User userIdentifier username username_email createdAt
