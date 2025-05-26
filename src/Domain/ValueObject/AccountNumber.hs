5e827f92e4eb0eba54386b54741bb76bb4fb9279
Yuri Melo <yurifullstack@gmail.com>
2025-05-26 19:05:04 -0300

    Add function in order to create AccountNumber with some validations

--- src/Domain/ValueObject/AccountNumber.hs
+++ src/Domain/ValueObject/AccountNumber.hs
@@ -1,3 +1,10 @@
 module Domain.ValueObject.AccountNumber where
 
 newtype AccountNumber = AccountNumber String deriving (Show, Eq)
+
+mkAccountNumber :: String -> Either String AccountNumber
+mkAccountNumber value
+  | not (all (`elem` ['0'..'9']) value) = Left "Account number must be numeric"
+  | length value /= 10 = Left "Account number must be exactly 10 digits"
+  | otherwise = Right (AccountNumber value)
+  
