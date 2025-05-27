d036205c1f4614c2e5df8b0764e00e3c8c4f7295
Yuri Melo <yurifullstack@gmail.com>
2025-05-26 21:53:10 -0300

    Translate from Portuguese to English

--- test/Domain/ValueObject/PhoneSpec.hs
+++ test/Domain/ValueObject/PhoneSpec.hs
@@ -9,6 +9,6 @@ spec = describe "formatPhone" $ do
     let phone = Phone "55" "11" "999998888" (Just "1234") Work
     formatPhone phone `shouldBe` "+55 (11) 999998888 ext. 1234 [Work]"
 
-  it "deve parsear número com extensão usando 'x'" $ do
+  it "it must parse number with extension 'x'" $ do
     parsePhone "5511999998888x1234"
       `shouldBe` Right (Phone "55" "11" "999998888" (Just "1234") Mobile)
